7bce9cb95c6d634e4a4cfea031fc81172267df5d
KaranBPatel <pkarantanvi@gmail.com>
2026-06-17 21:44:42 +0530

    fix; env set

--- config/database.php
+++ config/database.php
@@ -61,12 +61,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),  // This will parse the URL
+            'url' => 'postgresql://postgres:vYmiWSaBDdZVuLItHkjVhuKLtGcfEKlm@postgres.railway.internal:5432/railway',  // This will parse the URL
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'port' => 'postgres.railway.internal',
+            'database' => 'railway',
+            'username' => 'postgres',
+            'password' => 'vYmiWSaBDdZVuLItHkjVhuKLtGcfEKlm',
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
